8faf21bad6578f4e5737fdcfafff5bd181eee672
rmerzak <rmerzak@student.1337.ma>
2022-04-30 15:43:47 +0000

    fixing the error

--- etherjs/Event_Stream.js
+++ etherjs/Event_Stream.js
@@ -4,18 +4,21 @@ const INFURA_ID ='6e88ce3571b54d10aada72c1a629d9ba';
 const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`);
 
 const ERC20_ABI = [
-    "function name() view reaturns (string)",
+    "function name() view returns (string)",
     "function symbol() view returns (string)",
     "function totalSupply() view returns (uint256)",
     "function balanceOf(address) view returns (uint)",
-    "event Transfer(address indexed from,address indexed to, uint amount)"
+    "event Transfer(address indexed from, address indexed to, uint amount)"
 ]
+
 const address = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
 const contract = new ethers.Contract(address, ERC20_ABI, provider);
 
 const main = async () => {
     const block = await provider.getBlockNumber();
     const transferEvents = await contract.queryFilter('Transfer',block - 1, block);
-    console.log(transferEvents);
+    console.log(transferEvents[0].args);
 }
+
 main();
+ 
--- etherjs/Write_Contract.js
+++ etherjs/Write_Contract.js
@@ -7,8 +7,8 @@ const provider = new ethers.providers.JsonRpcProvider(`https://kovan.infura.io/v
 const acount2 = '0x141E6c2e5A72d14a25869d9b5C0C75df2741eD60';
 const acount1 = '0xC8E52c150EcaD9f1C072354d99086D7E7b740c9b';
 
-const PrivateKey1 = '45550d1fa3997cf22676ec41737378c7e36c534f3eac713ce98274c7ba96f239';
-const PrivateKey2 = '18f7726a4689d706a2e17557b64b01e0c5af7673b40c1a307c46960f689d7e00';
+const PrivateKey1 = '45551d1fa3997cf22676ec41737378c7e36c534f3eac713ce98274c7ba96f239'; //this is not my private key so don't bother yourself
+const PrivateKey2 = '18f7726a4689d716a2e17557b64b01e0c5af7673b40c1a307c46960f689d7e00'; //this is not my private key so don't bother yourself
 
 const wallet = new ethers.Wallet(PrivateKey2,  provider);
 
